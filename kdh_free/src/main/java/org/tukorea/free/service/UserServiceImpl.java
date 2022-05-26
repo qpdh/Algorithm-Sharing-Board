3e06c17a1d8d727e1335c42b1ac5445577032dd0
qpdh <74190329+qpdh@users.noreply.github.com>
2022-05-26 14:03:38 +0900

    done: bycrypt 암호화 적용

--- kdh_free/src/main/java/org/tukorea/free/service/UserServiceImpl.java
+++ kdh_free/src/main/java/org/tukorea/free/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package org.tukorea.free.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.tukorea.free.domain.UserVO;
 import org.tukorea.free.persistence.UserDAO;
@@ -24,6 +25,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void addUser(UserVO user) throws Exception {
+        String password = user.getPasswd();
+
+        // 디비에 저장할 비밀번호 암호화
+        String encrypted = BCrypt.hashpw(password, BCrypt.gensalt());
+
+        user.setPasswd(encrypted);
+
         userDAO.add(user);
     }
 
